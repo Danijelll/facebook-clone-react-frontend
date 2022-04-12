c65f73f11c8dcfcfe033f21d9fee3a9207a5f9eb
Danijel Lovas <danijellovas@gmail.com>
2022-04-12 12:46:46 +0200

    Add login form css

--- facebook-clone/src/pages/Login/Login.tsx
+++ facebook-clone/src/pages/Login/Login.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import './Login.scss'
 
 function Login() {
   return (
     <div className='wrapper'>
         
-        <input placeholder='Username...' />
-        <input type="password" placeholder='Password...' />
+        <input className="input" placeholder='Username...' />
+        <input className="input" type="password" placeholder='Password...' />
+        <button onClick={ () => {console.log("asd");
+        }} className="button">Login</button>
+        <Link className="link" to="/register">Dont have an account? Register</Link>
 
     </div>
   )
