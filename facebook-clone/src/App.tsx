ed3a4b1c2b7f5432747e6c22007a2965206df433
Danijel Lovas <danijellovas@gmail.com>
2022-04-12 11:04:03 +0200

    initial setup

--- facebook-clone/src/App.tsx
+++ facebook-clone/src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Home from './pages/Home/Home';
+
+const store = configureStore({
+  reducer:{
+
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-     <h1>AAA</h1>
-    </div>
+    <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
+    </Router>
+    </Provider>
   );
 }
 
---
+++ facebook-clone/src/pages/Login/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='wrapper'>
+        
+        <input placeholder='Username...' />
+        <input type="password" placeholder='Password...' />
+
+    </div>
+  )
+}
+
+export default Login
