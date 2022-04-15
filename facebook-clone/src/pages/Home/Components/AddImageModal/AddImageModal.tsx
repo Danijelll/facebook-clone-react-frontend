819a6085ce340081c345da7f1acd533bb944fc91
Danijel Lovas <danijellovas@gmail.com>
2022-04-15 14:40:29 +0200

    Add add image modal css

--- facebook-clone/src/pages/Home/Components/AddImageModal/AddImageModal.tsx
+++ facebook-clone/src/pages/Home/Components/AddImageModal/AddImageModal.tsx
@@ -1,11 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { RootStore } from '../../../../features/store';
 import './AddImageModal.scss'
 
 function AddImageModal({ closeModal }: { closeModal: any }) {
 
-    
+    const [images, setImages] = useState<FileList | null>(null);
+
+    const userData = useSelector((state: RootStore) => state.user.currentUser);
+
+
+    const handleImages = (e: React.FormEvent<HTMLInputElement>) => {
+        const files = (e.target as HTMLInputElement).files;
+        setImages(files);
+    }
+
+
+
     return (
-        <div className='modal-backgorund' onClick={() => closeModal(false)}>
+        <div className='modal-background' onClick={() => closeModal(false)}>
             <div id='modal-container' onClick={(e) => e.stopPropagation()}>
                 <button id='close-modal-button' onClick={() => closeModal(false)}> X </button>
 
@@ -14,19 +27,15 @@ function AddImageModal({ closeModal }: { closeModal: any }) {
                 </div>
 
                 <div id='body'>
-                    <input type="file" multiple name="" id="" />
+                    <input id='file-input' type="file" multiple onChange={handleImages} />
                 </div>
 
                 <div id='footer'>
+                    <button id='upload-image'>Upload Images</button>
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
-
 export default AddImageModal
-
-function listenForOutsideClick(listening: boolean, setListening: React.Dispatch<React.SetStateAction<boolean>>, menuRef: React.MutableRefObject<null>, setIsOpen: React.Dispatch<React.SetStateAction<boolean>>): React.EffectCallback {
-    throw new Error('Function not implemented.');
-}
