7ad40655010accb097595e621a4d1c3d0655ca65
Danijel Lovas <danijellovas@gmail.com>
2022-04-27 14:23:16 +0200

    Add css changes

--- facebook-clone/src/pages/ImageCarousel/Components/CommentModal/CommentModal.tsx
+++ facebook-clone/src/pages/ImageCarousel/Components/CommentModal/CommentModal.tsx
@@ -1,26 +1,33 @@
-import React, { useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { closeCommentModal } from '../../../../features/Ui/UiSlice'
+import './CommentModal.scss'
 
 
 function CommentModal() {
-    const dispatch = useDispatch();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log();
+    
+  }, [])
+  
 
   return (
-    <div className='modal-background' onClick={() => dispatch(closeCommentModal())}>
-    <div id='modal-container' onClick={(e) => e.stopPropagation()}>
-        <button id='close-modal-button' onClick={() => dispatch(closeCommentModal())}> X </button>
+    <div className='comment-modal-background' onClick={() => dispatch(closeCommentModal())}>
+      <div id='comment-modal-container' onClick={(e) => e.stopPropagation()}>
 
         <div id='body'>
-            
+
         </div>
 
         <div id='footer'>
-           
+
         </div>
 
-    </div>
-</div >
+      </div>
+    </div >
   )
 }
 
--- facebook-clone/src/pages/ImageCarousel/ImageCarousel.tsx
+++ facebook-clone/src/pages/ImageCarousel/ImageCarousel.tsx
@@ -20,7 +20,6 @@ function ImageCarousel(props: ImageCarouselProps) {
     const { images, captions, createdOn } = props;
     const dispatch = useDispatch();
 
-
     const loadImages = () => {
         return images?.map(image => <img height="450px" width="960px" key={image.imageUrl} src={image.imageUrl} alt={image.imageUrl} />)
     }
@@ -35,36 +34,38 @@ function ImageCarousel(props: ImageCarouselProps) {
     };
 
     return (
-        
-        <div id='image-slider-body' >
-            {setShowCommentModal && <CommentModal/>}
-            <div id='image-slider-header'>
-                <img id='image-slider-user-image' src={userData?.profileImage} alt="" />
-                <p id='image-slider-username'>{userData?.username}</p>
-                <div onClick={() => dispatch(showCommentModal())} id='image-slider-add-comment-button'>
-                    <img id='image-slider-add-comment-button-svg' src="comment.svg" alt="commentSvg" />
+        <div>
+            {setShowCommentModal && <CommentModal />}
+
+            <div id='image-slider-body' >
+                <div id='image-slider-header'>
+                    <img id='image-slider-user-image' src={userData?.profileImage} alt="" />
+                    <p id='image-slider-username'>{userData?.username}</p>
+                    <div onClick={() => dispatch(showCommentModal())} id='image-slider-add-comment-button'>
+                        <img id='image-slider-add-comment-button-svg' src="comment.svg" alt="commentSvg" />
+                    </div>
+                </div>
+                <div id='image-slider-created-on'>
+                    {createdOn.toString().slice(0, 10)}
+                </div>
+                <Slider className='image-slider' {...settings}>
+                    {loadImages()}
+                </Slider>
+                <div id='image-slider-comments'>
+                    {captions &&
+                        <div id='image-slider-caption-wrapper'>
+                            <p id='image-slider-caption-username'>
+                                {userData?.username}
+                            </p>
+                            <p>
+                                {captions}
+                            </p>
+                        </div>
+                    }
                 </div>
             </div>
-            <div id='image-slider-created-on'>
-                {createdOn.toString().slice(0, 10)}
-            </div>
-            <Slider className='image-slider' {...settings}>
-                {loadImages()}
-            </Slider>
-            <div id='image-slider-comments'>
-                {captions &&
-                    <div id='image-slider-caption-wrapper'>
-                        <p id='image-slider-caption-username'>
-                            {userData?.username}
-                        </p>
-                        <p>
-                            {captions}
-                        </p>
-                    </div>
-                }
-            </div>
-        </div>
 
+        </div>
     )
 }
 
